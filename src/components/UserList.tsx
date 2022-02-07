249a9075b27c773f8be49cba2674fa363f849367
uniqcle <uniqcle@yandex.ru>
2022-02-07 12:35:19 +0700

    хук UseActions

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import { fetchUsers } from '../store/action-creators/user';
+import { useActions } from './../hooks/useActions';
 
 const UserList: React.FC = () => {
     const {users, error, loading} = useTypedSelector(state => state.user); 
-    const dispatch = useDispatch(); 
+     const {fetchUsers} = useActions(); 
 
     useEffect(()=> {
-        dispatch(fetchUsers())
+        fetchUsers()
     }, [])
 
     if(loading){
